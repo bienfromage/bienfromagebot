d8dc91b36242e38785e160bdaf71639fe55590b4
bienfromage <100codemaster@gmail.com>
2018-02-19 17:03:38 -0500

    added port

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const Discord = require("discord.js");
 
 var http = require('http');
+const PORT = process.env.PORT || 3000;
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
